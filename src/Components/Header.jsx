55a7569d643936d223c152070e106a5a6803e595
nithigsn <nithishgsn000@gmail.com>
2024-08-09 20:36:44 +0530

    updated pics

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,28 +1,24 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { userContext } from "../App";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 export default function Header() {
+
     const [openMenu, setOpenMenu] = useState(false);
 
-    const { home, setHome } = useContext(userContext);
+    const { home, setHome ,tab, setTab } = useContext(userContext);
 
-    const [tab, setTab] = useState(() => {
-        const savedTab = localStorage.getItem('tab');
-        return savedTab !== null ? JSON.parse(savedTab) : "home"
-    })
+
+
 
     const navigate = useNavigate();
+    
+    const location = useLocation();
 
-    function handleNavigateHome() {
-        navigate('/');
-        setHome(true);
-    }
 
     function handleTab() {
-
         setTab("work");
         navigate('/extra');
     }
@@ -34,11 +30,11 @@ export default function Header() {
         <div className="w-full h-24 flex items-center justify-evenly mt-4 relative">
             <div className="h-[60px] w-[60px] rounded-[50%] bg-[#ffffff] bsh flex items-center justify-center">
                 {
-                    home ? <div className="bitmoji"></div> : <i className="fa-solid fa-arrow-left cursor-pointer" style={{ color: "#d4d6d8" }} onClick={handleNavigateHome} > </i>
+                    location.pathname === '/' ? <div className="bitmoji"></div> : <i className="fa-solid fa-arrow-left cursor-pointer" style={{ color: "#d4d6d8" }} onClick={() => navigate(-1)} > </i>
                 }
             </div>
-            <div className="h-[60px] w-[186px] bg-[#ffffff] bsh rounded-[30px] flex items-center justify-evenly">
-                <div className={`h-12 w-20 rounded-[20px] text-center text-sm flex items-center justify-center sm:text-base  ${tab === home ? "currentTab" : "noTab"}`} onClick={()=>navigate('/')}>Work</div>
+            <div className="h-[60px] w-[186px] bg-[#ffffff] bsh rounded-[30px] flex items-center justify-evenly ">
+                <div className={`h-12 w-20 rounded-[20px] text-center text-sm flex items-center justify-center sm:text-base  ${tab === home ? "currentTab" : "noTab"}`} onClick={() => navigate('/')}>Work</div>
                 <div className={`h-12 w-20 rounded-[20px] text-center text-sm flex items-center justify-center sm:text-base  ${tab}`} onCanPlay={handleTab}>Play</div>
             </div>
             <div className="h-[60px] w-[60px] rounded-[50%] bg-[#ffffff] bsh flex items-center justify-center" onClick={() => setOpenMenu(!openMenu)}>
--- src/Pages/Indian/FishtherVanissa.jsx
+++ src/Pages/Indian/FishtherVanissa.jsx
@@ -15,9 +15,9 @@ export default function FistherVanissa() {
 
         <div className="w-full h-full">
 
-            <div className="w-screen h-[300px] sm:h-[500px] lg:w-full lg:h-[900px] fi-bg flex justify-center">
+            <div className="w-screen h-[285px] sm:h-[500px] lg:w-full lg:h-[900px] fi-bg flex justify-center rounded-t-md lg:rounded-t-xl">
 
-                <h1 className="text-lg p-10 lg:p-[200px]" data-aos='fade-zoom-in'>Fisther & Vanissa</h1>
+                <h1 className="text-lg p-10 lg:text-4xl lg:p-[200px]" data-aos='fade-zoom-in'>Fisther & Vanissa</h1>
 
 
             </div>
