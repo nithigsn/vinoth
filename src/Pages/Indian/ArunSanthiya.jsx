018e09bb409b5686da2b0e361da05ff6c3942130
nithigsn <nithishgsn000@gmail.com>
2024-08-29 00:01:30 +0530

    adding images using cloudinary

--- src/Pages/Indian/ArunSanthiya.jsx
+++ src/Pages/Indian/ArunSanthiya.jsx
@@ -20,39 +20,88 @@ export default function ArunSanthiya() {
 
             <h1 className='he-wed text-6xl'>Arun Weds Santhiya</h1>
 
-            <div className="w-[65vw] h-full flex flex-gap-1 mt-8">
 
-                <div className="flex flex-col w-[32.5vw] h-[60vh]  gap-1 ">
-                    <div className="h-[20vh] w-[32.5vw]  flex gap-1">
+            <h3>Pre Wedding Shoots</h3>
 
-                        <div className="h-[20vh] w-[22vw] bg-land-1 small" data-aos="fade-right"  >
+            <div className="w-[90vw] h-full flex flex-col mt-8">
 
-                        </div>
 
-                        <div className="h-[20vh] w-[10.5vw] bg-sq3 small"></div>
+                <div className="flex  flex-col  gap-y-1">
+
+                    <div className='flex gap-x-1'>
+
+                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724859292/VIN_7670_sjvgg0.jpg" alt="" className='w-[45vw] h-[300px]' />
+
+                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724859289/VIN_7619_idna8z.jpg" alt="" className='w-[45vw] h-[300px]' />
+
                     </div>
 
-                    <div className="w-[32.5vw] h-[40vh] bg-land-2 big ">
+
+
+
+                    <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724859294/VIN_7782_ajuke1.jpg" alt="" className='w-full h-[250px]' />
+
+
+                    <div className="flex gap-x-1">
+
+                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724859295/VIN_7792_pbgc9d.jpg" alt="" className='w-[29.4vw] h-[200px]' />
+                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724859293/VIN_7744_rj6kef.jpg" alt="" className='w-[29.4vw] h-[200px]' />
+                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724859291/VIN_7664_xtdp6r.jpg" alt="" className='w-[29.4vw] h-[200px]' />
+
 
                     </div>
+
+                    <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724859294/VIN_7769_szw1r6.jpg" alt="" className='w-full h-[250px]' />
+
+
+                    <div className='flex gap-x-1'>
+
+                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724859289/VIN_7654_aiotss.jpg" alt="" className='w-[45vw] h-[300px]' />
+
+                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724859293/VIN_7709_c8d61i.jpg" alt="" className='w-[45vw] h-[300px]' />
+
+
+                    </div>
+
+
+
+
                 </div>
 
 
-                <div className="flex flex-col w-[32.5vw] h-[60vh] gap-1" >
-                    <div className="flex h-[40vh] w-full  gap-1">
-                        <div className="w-[10.83vw] h-[40vh] bg-ver big" data-aos='fade-zoom-in' ></div>
-                        <div className="w-[10.83vw] h-[40vh] bg-ver2 big"></div>
-                        <div className="w-[10.83vw] h-[40vh] flex flex-col gap-1">
-                            <div className="h-[20vh] bg-sq"></div>
-                            <div className="h-[20vh] bg-sq1"></div>
-                        </div>
-                    </div>
-                    <div className="w-[32.5vw] h-[20vh] flex gap-1">
-                        <div className="w-[15.83vw] h-[20vh] bg-sq2 bg-red-50"></div>
-                        <div className="w-full bg-land"></div>
+
+                <h2 className='text-center mt-10'>Wedding Shoot</h2>
+
+
+                <div className='w-[90vw] h-full flex flex-col mt-8'>
+
+                    <div className='flex flex-col gap-y-1'>
+
+                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724859290/9W8A7079_twmyje.jpg" alt="" />
+                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724859290/9W8A7085_thgoyq.jpg" alt="" />
+
+                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724859288/9W8A6903_zff19k.jpg" alt="" srcset="" />
+
+
                     </div>
 
                 </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
             </div>
