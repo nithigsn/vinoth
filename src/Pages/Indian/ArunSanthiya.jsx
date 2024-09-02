561389e0d592eb42b4cb8862d8dd879e30281860
nithigsn <nithishgsn000@gmail.com>
2024-09-02 15:07:11 +0530

    animated aos

--- src/Pages/Indian/ArunSanthiya.jsx
+++ src/Pages/Indian/ArunSanthiya.jsx
@@ -18,7 +18,7 @@ export default function ArunSanthiya() {
         <div className="w-full h-full flex flex-col items-center  ">
 
 
-            <h1 className='he-wed text-6xl'>Arun Weds Santhiya</h1>
+            <h1 className=' text-2xl'>Arun Weds Santhiya</h1>
 
 
             <h3>Pre Wedding Shoots</h3>
@@ -30,27 +30,27 @@ export default function ArunSanthiya() {
                     <div className='flex gap-1 flex-wrap  '>
 
 
-                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724859292/VIN_7670_sjvgg0.jpg" alt="" className='w-[188.5px] h-[300px] lg:w-[250px] lg:h-[400px] ' />
-                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724859289/VIN_7619_idna8z.jpg" alt="" className='w-[188.5px] h-[300px] lg:w-[250px] lg:h-[400px] ' />
-                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724859289/VIN_7654_aiotss.jpg" alt="" className='w-[188.5px] h-[300px] lg:w-[250px] lg:h-[400px] ' />
-                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724859293/VIN_7709_c8d61i.jpg" alt="" className='w-[188.5px] h-[300px] sm:hidden md:block lg:w-[250px] lg:h-[400px] ' />
+                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724859292/VIN_7670_sjvgg0.jpg" alt="" className='w-[188.5px] h-[300px] lg:w-[250px] lg:h-[400px] '  data-aos="zoom-in"/>
+                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724859289/VIN_7619_idna8z.jpg" alt="" className='w-[188.5px] h-[300px] lg:w-[250px] lg:h-[400px] '  data-aos="zoom-in"/>
+                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724859289/VIN_7654_aiotss.jpg" alt="" className='w-[188.5px] h-[300px] lg:w-[250px] lg:h-[400px] '  data-aos="zoom-in"/>
+                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724859293/VIN_7709_c8d61i.jpg" alt="" className='w-[188.5px] h-[300px] sm:hidden md:block lg:w-[250px] lg:h-[400px] '  data-aos="zoom-in"/>
 
 
                     </div>
 
                     <div className="flex gap-1 flex-col lg:flex-row">
 
-                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724859294/VIN_7782_ajuke1.jpg" alt="" className='w-full h-[250px] sm:h-[350px] lg:w-[504px] lg:h-[350px]' />
-                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724926513/VIN_7674_bq7sza.jpg" alt="" className='w-full h-[250px] sm:h-[350px] lg:h-[350px] lg:w-[504px] ' />
+                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724859294/VIN_7782_ajuke1.jpg" alt="" className='w-full h-[250px] sm:h-[350px] lg:w-[504px] lg:h-[350px]'  data-aos="fade-right"/>
+                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724926513/VIN_7674_bq7sza.jpg" alt="" className='w-full h-[250px] sm:h-[350px] lg:h-[350px] lg:w-[504px]'  data-aos="fade-left"/>
 
 
                     </div>
 
                     <div className="flex gap-x-1 ">
 
-                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724926509/VIN_7629_q4yozn.jpg" alt="" className='w-[29.4vw] h-[200px] sm:h-[285px] sm:w-[271px] lg:h-[500px] lg:w-[337px]' />
-                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724859291/VIN_7664_xtdp6r.jpg" alt="" className='w-[29.4vw] h-[200px] sm:h-[285px] sm:w-[271px] lg:h-[500px] lg:w-[337px]' />
-                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724859293/VIN_7744_rj6kef.jpg" alt="" className='w-[29.4vw] h-[200px] sm:h-[285px] sm:w-[271px] lg:h-[500px] lg:w-[337px]' />
+                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724926509/VIN_7629_q4yozn.jpg" alt="" className='w-[29.4vw] h-[200px] sm:h-[285px] sm:w-[271px] lg:h-[500px] lg:w-[337px]' data-aos='zoom-out' />
+                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724859291/VIN_7664_xtdp6r.jpg" alt="" className='w-[29.4vw] h-[200px] sm:h-[285px] sm:w-[271px] lg:h-[500px] lg:w-[337px]' data-aos='zoom-out' />
+                        <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724859293/VIN_7744_rj6kef.jpg" alt="" className='w-[29.4vw] h-[200px] sm:h-[285px] sm:w-[271px] lg:h-[500px] lg:w-[337px]' data-aos='zoom-out' />
 
 
                     </div>
@@ -93,9 +93,11 @@ export default function ArunSanthiya() {
 
                         <div className='flex gap-1 flex-wrap lg:justify-center'>
 
-                            <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724926526/9W8A6637_xvzmhu.jpg" className='w-[188.5px] h-[300px] lg:w-[250px] lg:h-[400px] ' alt="" />
+                            
                             <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724926528/9W8A7176_z5u5e1.jpg" className='w-[188.5px] h-[300px] lg:w-[250px] lg:h-[400px] ' alt="" />
                             <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724926525/9W8A6622_hondti.jpg" className='w-[188.5px] h-[300px] lg:w-[250px] lg:h-[400px] ' alt="" />
+                            <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1725268292/9W8A6293_ohhoay.jpg" className='w-[188.5px] h-[300px] lg:w-[250px] lg:h-[400px] ' alt="" />
+                           
 
                         </div>
 
@@ -113,14 +115,22 @@ export default function ArunSanthiya() {
 
                         <div className='flex gap-1 flex-wrap lg:justify-center'>
                             <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724926506/IMG_0097_omjrer.jpg" alt="" className=' w-[188.5px] h-[300px] lg:w-[250px] lg:h-[400px]' />
-                            <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724926525/9W8A6688_urzovm.jpg" alt="" className='w-[188.5px] h-[300px] lg:w-[250px] lg:h-[400px]' />
+                            <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1725268290/9W8A6543_ffpfm5.jpg" alt="" className='w-[188.5px] h-[300px] lg:w-[250px] lg:h-[400px]' />
                             <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724926519/9W8A6286_rczivh.jpg" alt="" className='w-[188.5px] h-[300px] lg:w-[250px] lg:h-[400px]' />
+                            <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1725268293/9W8A6492_lqyazf.jpg" alt="" className='w-[188.5px] h-[300px] lg:w-[250px] lg:h-[400px]' />
                         </div>
 
 
                         <div className='flex flex-col gap-[3px] lg:flex-row lg:flex-wrap'>
                             <img src="https://res.cloudinary.com/doqdlneyl/image/upload/c_fill,ar_16:9/v1724926519/9W8A6190_rm5fju.jpg" alt="" className='lg:w-[504px]' />
                             <img src="https://res.cloudinary.com/doqdlneyl/image/upload/c_fill,ar_16:9/v1724926506/IMG_9880_tqrkks.jpg" alt="" className='lg:w-[504px]' />
+                            <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1725268292/IMG_9852_qmpz6z.jpg"                alt="" className='lg:w-[504px]' />
+                            <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1725268295/IMG_9861_j3sl51.jpg" alt="" className='lg:w-[504px]' />
+
+
+
+
+
                             <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724926526/9W8A6299_mfnsoj.jpg" alt="" className='lg:w-[504px]' />
                             <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724926523/9W8A6568_jnl83x.jpg" alt="" className='lg:w-[504px]' />
                             <img src="https://res.cloudinary.com/doqdlneyl/image/upload/v1724926530/9W8A7786_sxjhqg.jpg" alt="" className='lg:w-[504px]' />
