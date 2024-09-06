bb7acefddc841d9471483ed2d53fe1784ca9759c
nithigsn <nithishgsn000@gmail.com>
2024-09-06 13:20:07 +0530

    added responsiveness

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -4,15 +4,15 @@ function About() {
     return (
         <div className='h-screen w-full flex justify-center'>
 
-            <div className="flex flex-col items-center w-[60vw] h-full">
+            <div className="flex w-[90vw] flex-col items-center lg:w-[60vw] h-full">
 
 
                 <h1 className='text-4xl'>  Hi again, I'm Vinoth.</h1>
 
-                <div className='w-[35vw] mt-5 '>
+                <div className=' w-[300px] lg:w-[35vw] mt-5 text-sm '>
 
                     <p>I'm a photographer and a videographer living in Paris,France.</p>
-                    <p>interested in how camera can capture moments and make people life better.</p>
+                    <p>interested in how camera can capture moments and make people feel better.</p>
                     <p className='mt-10'>   I started taking photo  honing my visual design skills and learning the ins and outs of every frame, then moved into the world of capturing moments.</p>
                     <p className='mt-10'> As a designer, I enjoy being a generalist with a focus on craft and prototyping. I value beautiful visuals backed by smart systems thinking. I love the concept that a capturing moments is a living thing that can adapt and evolve over time. <br /></p>
                     <p className='mt-10'> Outside of design, I love food in all capacities. I spend my time exploring new restaurants, testing new products.If you want to chat, feel free to reach out.</p>
