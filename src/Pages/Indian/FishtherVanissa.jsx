c2aa04fda8d6c180a993b894b8af6a2dcf892e62
nithigsn <nithishgsn000@gmail.com>
2024-09-07 14:02:48 +0530

    updated heading

--- src/Pages/Indian/FishtherVanissa.jsx
+++ src/Pages/Indian/FishtherVanissa.jsx
@@ -17,7 +17,7 @@ export default function FistherVanissa() {
 
             <div className="w-screen h-[285px] sm:h-[500px] lg:w-full lg:h-[900px] fi-bg flex justify-center rounded-t-md lg:rounded-t-xl" data-aos='zoom-in'>
 
-                <h1 className="text-lg p-10 lg:text-4xl lg:p-[200px]" data-aos='zoom-in'>Fisther & Vanissa</h1>
+                <h1 className="text-lg p-10 lg:text-4xl lg:p-[200px]" data-aos='zoom-in'>Fisther Weds Vanissa</h1>
 
 
             </div>
