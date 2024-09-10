7cecf528366af1ff69a8f848bd4b51dfd7115bd5
nithigsn <nithishgsn000@gmail.com>
2024-09-10 12:57:38 +0530

    added bg color to events

--- src/Pages/Events.jsx
+++ src/Pages/Events.jsx
@@ -23,14 +23,15 @@ export default function Events() {
 
             <div className="indian w-[80vw] h-full flex flex-col gap-x-1 mt-8 items-center justify-center lg:flex-row lg:gap-x-3">
 
-                <div className='indian-inner h-[500px] w-[80vw]  text-white flex flex-col items-center justify-end bg-ver sm:h-[650px] sm:w-[60vw]  ' data-aos='fade-right'>
-                    <div className='flex flex-col h-[200px] justify-center items-center '>
+                <div className='indian-inner h-[500px] w-[80vw]  text-white flex flex-col items-center justify-end celine sm:h-[650px] sm:w-[60vw] lg:h-[700px] ' data-aos='fade-right'>
+                    <div className='flex flex-col h-[200px] justify-center items-center w-full bg-glass'>
                         <h3 className='text-xl font-semibold sm:text-2xl'>Celine</h3>
-                        <p className='text-xs w-[200px] text-center sm:w-[400px]'> It was a surprise birthday party with a lot of unexpected peopl flew from different countries to surprise her.</p>
-                        <button className=' mt-6 h-8 w-3/4 border text-xs rounded cursor-pointer ' onClick={()=>navigate('/events/celine')} >Captured Moments</button>
+                        <p className='text-xs w-[200px] text-black font-medium lg:text-[15px] text-center sm:w-[400px]'> It was a surprise birthday party with a lot of unexpected people flew from different countries to surprise her.</p>
+                        <button className=' mt-6 h-8 w-3/4 border text-xs rounded cursor-pointer text-black ' onClick={()=>navigate('/events/celine')} >Captured Moments</button>
 
                     </div>
                 </div>
+
                
 
             </div>
