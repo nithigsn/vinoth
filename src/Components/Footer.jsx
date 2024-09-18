e92f1c0d4df2b35f2822ff8241b11ce8d86e25e4
nithigsn <nithishgsn000@gmail.com>
2024-09-18 11:59:48 +0530

    added tiktok

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -10,7 +10,7 @@ export default function Footer() {
 
                 <a href="mailto:vinothyaso17@gmail.com?subject=Hello%20There" className='flex items-center gap-1'><i className="fa-solid fa-envelope text-2xl cursor-pointer pl-5" style={{ color: "#c7cfdb", }}></i> </a>
                 <a href="https://www.instagram.com/vinoth_jeyasabesan" target='blank' className='flex items-center gap-1'><i className="fa-brands fa-instagram text-2xl cursor-pointer" style={{ color: "#c7cfdb", }}></i> </a>
-                <a href="mailto:vinothyaso17@gmail.com?subject=Hello%20There" className='flex items-center gap-1'><i className="fa-brands fa-tiktok text-2xl cursor-pointer pl-7" style={{ color: "#c7cfdb", }}></i> </a>
+                <a href="https://www.tiktok.com/@vinothjeyasabesan" className='flex items-center gap-1'><i className="fa-brands fa-tiktok text-2xl cursor-pointer pl-7" style={{ color: "#c7cfdb", }}></i> </a>
                 <a href="https://www.youtube.com/@vinothjeyasbesan" className='flex items-center gap-1'><i className="fa-brands fa-youtube text-2xl cursor-pointer pl-3" style={{ color: "#c7cfdb", }}></i> </a>
 
 
