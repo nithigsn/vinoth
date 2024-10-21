2c2d2ce66227ff57178c233602be52c359744998
nithigsn <nithishgsn000@gmail.com>
2024-10-21 12:22:53 +0530

    updated footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,12 +6,12 @@ export default function Footer() {
 
 
         <div className="flex flex-col w-full h-[200px] items-center justify-evenly bgmain mt-20 relative bottom-0 ">
-            <div className="inline-flex w-[300px] justify-around">
+            <div className="inline-flex w-[300px] justify-evenly">
 
-                <a href="mailto:vinothyaso17@gmail.com?subject=Hello%20There" className='flex items-center gap-1'><i className="fa-solid fa-envelope text-2xl cursor-pointer pl-5" style={{ color: "#c7cfdb", }}></i> </a>
-                <a href="https://www.instagram.com/vinoth_jeyasabesan" target='blank' className='flex items-center gap-1'><i className="fa-brands fa-instagram text-2xl cursor-pointer" style={{ color: "#c7cfdb", }}></i> </a>
-                <a href="https://www.tiktok.com/@vinothjeyasabesan" className='flex items-center gap-1'><i className="fa-brands fa-tiktok text-2xl cursor-pointer pl-7" style={{ color: "#c7cfdb", }}></i> </a>
-                <a href="https://www.youtube.com/@vinothjeyasbesan" className='flex items-center gap-1'><i className="fa-brands fa-youtube text-2xl cursor-pointer pl-3" style={{ color: "#c7cfdb", }}></i> </a>
+                <a href="mailto:vinothyaso17@gmail.com?subject=Hello%20There" className='flex items-center justify-center gap-1'><i className="fa-solid fa-envelope text-2xl cursor-pointer pl-5" style={{ color: "#c7cfdb", }}></i> </a>
+                <a href="https://www.instagram.com/vinoth_jeyasabesan" target='blank' className='flex items-center justify-center gap-1'><i className="fa-brands fa-instagram text-2xl cursor-pointer" style={{ color: "#c7cfdb", }}></i> </a>
+                <a href="https://www.tiktok.com/@vinothjeyasabesan" className='flex items-center justify-center gap-1'><i className="fa-brands fa-tiktok text-2xl cursor-pointer " style={{ color: "#c7cfdb", }}></i> </a>
+                <a href="https://www.youtube.com/@vinothjeyasbesan" className='flex items-center justify-center gap-1'><i className="fa-brands fa-youtube text-2xl cursor-pointer" style={{ color: "#c7cfdb", }}></i> </a>
 
 
             </div>
