64dd87a8e180d46c2e9cb6a82df646b207e7ecf8
nithigsn <nithishgsn000@gmail.com>
2024-11-08 20:48:21 +0530

    updated spline

--- src/Components/SplineHome.jsx
+++ src/Components/SplineHome.jsx
@@ -5,7 +5,10 @@ const SplineHome = () => {
     return (
         <div className='h-screen w-full absolute top-0  '>
 
-            <Spline scene="https://prod.spline.design/B9KWYpypXgkDVM5O/scene.splinecode" />
+            {/* <Spline scene="https://prod.spline.design/B9KWYpypXgkDVM5O/scene.splinecode" /> */}
+
+            
+            <Spline scene="https://prod.spline.design/1s1BcczaPnRGoqFO/scene.splinecode" />
 
 
             
